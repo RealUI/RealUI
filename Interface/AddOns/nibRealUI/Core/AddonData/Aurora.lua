e1a0e1a967db13b2f98c69e7fb26089dc02a9c30
Nibelheim <nibelheim.int@gmail.com>
2013-09-11 03:17:14 +1000

    Reset

--- Interface/AddOns/nibRealUI/Core/AddonData/Aurora.lua
+++
@@ -1,5 +0,0 @@
-local nibRealUI = LibStub("AceAddon-3.0"):GetAddon("nibRealUI")
-
-nibRealUI.LoadAddOnData_Aurora = function()
-	AuroraConfig = nil
-end
--- Interface/AddOns/nibRealUI/Core/AddonData/FreebTip.lua
+++
@@ -1,9 +0,0 @@
-local nibRealUI = LibStub("AceAddon-3.0"):GetAddon("nibRealUI")
-
-nibRealUI.LoadAddOnData_FreebTip = function()
-	FreebTipDB = {
-		["y"] = 192,
-		["x"] = -31,
-		["point"] = "BOTTOMRIGHT",
-	}
-end
