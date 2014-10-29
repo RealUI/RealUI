ac3d48ab65b38f0cfddbb0ebd96908b156a9f843
Nibelheim <nibelheim.int@gmail.com>
2014-10-30 05:35:32 +1000

    Fix Nameplate font scaling

--- Interface/AddOns/!Aurora_RealUI/AddOns/KNP.lua
+++ Interface/AddOns/!Aurora_RealUI/AddOns/KNP.lua
@@ -5,28 +5,28 @@ local MODNAME = "KNP"
 local KNP = nibRealUI:NewModule(MODNAME, "AceEvent-3.0", "AceTimer-3.0")
 local kuiNP
 local knUpdateScheduled = false
-local HeightToScale = {
-	["720"] = 0.95,
-	["768"] = 0.89,
-	["900"] = 0.76,
-	["960"] = 0.72,
-	["1024"] = 0.67,
-	["1050"] = 0.66,
-	["1080"] = 0.64,
-	["1200"] = 0.57,
-	["1440"] = 0.48,
-	["1600"] = 0.43,
-}
+-- local HeightToScale = {
+-- 	["720"] = 0.95,
+-- 	["768"] = 0.89,
+-- 	["900"] = 0.86,
+-- 	["960"] = 0.72,
+-- 	["1024"] = 0.67,
+-- 	["1050"] = 0.66,
+-- 	["1080"] = 0.64,
+-- 	["1200"] = 0.63,
+-- 	["1440"] = 0.48,
+-- 	["1600"] = 0.43,
+-- }
 function KNP:UpdateKNPFontScale()
 	if IsAddOnLoaded("Kui_Nameplates") and nibRealUI:GetModuleEnabled(MODNAME) then
 		if kuiNP.db.profile then
 			local screenHeight = string.match(({GetScreenResolutions()})[GetCurrentResolution()], "%d+x(%d+)")
 			local scale
-			if HeightToScale[screenHeight] then
-				scale = HeightToScale[screenHeight]
-			else
+			-- if HeightToScale[screenHeight] then
+				-- scale = HeightToScale[screenHeight]
+			-- else
 				scale = ceil(((768 * (nibRealUI.font.pixel1[2] / kuiNP.defaultSizes.font.name)) / screenHeight) * 100) / 100
-			end
+			-- end
 
 			kuiNP.db.profile.fonts.options.fontscale = scale
 			kuiNP:ScaleSizes("font")
@@ -67,7 +67,6 @@ function KNP:UpdateFonts()
 end
 
 function KNP:OnInitialize()
-	
 	self:SetEnabledState(nibRealUI:GetModuleEnabled(MODNAME))
 	nibRealUI:RegisterSkin(MODNAME, "KuiNameplates")
 end
