b7672b133e25c5b83123a737df12172049cbac7f
Gethe <gethe88@gmail.com>
2014-10-30 14:05:11 -0500

    skins for Clique and EasyMail

---
+++ Interface/AddOns/!Aurora_RealUI/AddOns/EasyMail.lua
@@ -0,0 +1,89 @@
+local _, mods = ...
+
+mods["EasyMail"] = function(F, C)
+    --print("HELLO EasyMail!!!", F, C)
+    local _, class = UnitClass("player")
+    local r, g, b = C.classcolours[class].r, C.classcolours[class].g, C.classcolours[class].b
+
+    local function SetTexture(f, name)
+        f:SetNormalTexture([[Interface\AddOns\!Aurora_RealUI\Media\AddOns\EasyMail\]]..name)
+        local nTex = f:GetNormalTexture()
+        nTex:SetVertexColor(r, g, b)
+
+        f:SetPushedTexture([[Interface\AddOns\!Aurora_RealUI\Media\AddOns\EasyMail\]]..name) --..[[Down]])
+        local pTex = f:GetPushedTexture()
+        pTex:SetVertexColor(r, g, b)
+
+        f:SetDisabledTexture([[Interface\AddOns\!Aurora_RealUI\Media\AddOns\EasyMail\]]..name) --..[[Disabled]])
+    end
+
+    -- Inbox
+    F.Reskin(EasyMail_CheckAllButton)
+    SetTexture(EasyMail_CheckAllButton, "CheckAll")
+
+    F.Reskin(EasyMail_ClearAllButton)
+    SetTexture(EasyMail_ClearAllButton, "ClearAll")
+
+    F.Reskin(EasyMail_CheckPageButton)
+    SetTexture(EasyMail_CheckPageButton, "CheckPage")
+
+    F.Reskin(EasyMail_ClearPageButton)
+    SetTexture(EasyMail_ClearPageButton, "ClearPage")
+
+    F.Reskin(EasyMail_GetAllButton)
+    SetTexture(EasyMail_GetAllButton, "GetChecked")
+    local bag = EasyMail_GetAllButton:CreateTexture()
+    bag:SetPoint("TOPRIGHT", -14, -7)
+    bag:SetSize(20, 20)
+    bag:SetTexture([[Interface\Icons\INV_Misc_Bag_08]])
+    bag:SetTexCoord(0.08, 0.92, 0.08, 0.92)
+    bag:Show()
+    hooksecurefunc(EasyMail_GetAllButton, "Disable", function()
+        --print("Disable", bag)
+        bag:SetDesaturated(true)
+    end)
+    hooksecurefunc(EasyMail_GetAllButton, "Enable", function()
+        --print("Enable", bag)
+        bag:SetDesaturated(false)
+    end)
+
+    hooksecurefunc(EasyMail, "InboxUpdate", function()
+        for i = 1, INBOXITEMS_TO_DISPLAY do
+            local check = _G["EasyMail_CheckButton"..i]
+            if check and not check.skinned then
+                F.ReskinCheck(check)
+            end
+        end
+    end)
+
+
+    -- Send
+    F.Reskin(EasyMail_MailButton, true)
+    EasyMail_MailButton:SetSize(20, 20)
+    EasyMail_MailButton:ClearAllPoints()
+    EasyMail_MailButton:SetPoint("TOPLEFT", SendMailNameEditBox, "TOPRIGHT", -1, 0)
+
+    local tex = EasyMail_MailButton:CreateTexture(nil, "ARTWORK")
+    tex:SetTexture(C.media.arrowDown)
+    tex:SetSize(8, 8)
+    tex:SetPoint("CENTER")
+    tex:SetVertexColor(1, 1, 1)
+    EasyMail_MailButton.tex = tex
+
+    EasyMail_MailButton:HookScript("OnEnter", F.colourArrow)
+    EasyMail_MailButton:HookScript("OnLeave", F.clearArrow)
+
+
+    F.CreateBD(EasyMail_MailDropdownBackdrop)
+    for i = 1, 15 do
+        local highlight = _G["EasyMail_MailDropdownButton" .. i .. "Highlight"]
+        highlight:SetTexture(r, g, b, .8)
+        highlight:ClearAllPoints()
+        highlight:SetPoint("TOPLEFT", -11, 0)
+        highlight:SetPoint("BOTTOMRIGHT", 12, 0)
+    end
+
+    -- Open
+    F.Reskin(EasyMail_AttButton)
+    F.Reskin(EasyMail_ForwardButton)
+end
--- Interface/AddOns/!Aurora_RealUI/Misc/MiscSkins.lua
+++ Interface/AddOns/!Aurora_RealUI/Misc/MiscSkins.lua
@@ -1,27 +1,6 @@
 local _, mods = ...
 
-mods["Clique"] = function(F, C)
-    if not CliqueSpellTab then return end
-    print("HELLO Clique!!!", F, C)
-    local tab = CliqueSpellTab
-    F.ReskinTab(tab)
-
-    tab:SetCheckedTexture(C.media.checked)
-    local hl = tab:GetHighlightTexture()
-    hl:SetPoint("TOPLEFT", 0, 0)
-    hl:SetPoint("BOTTOMRIGHT", 0, 0)
-
-    local bg = CreateFrame("Frame", nil, tab)
-    bg:SetPoint("TOPLEFT", -1, 1)
-    bg:SetPoint("BOTTOMRIGHT", 1, -1)
-    bg:SetFrameLevel(tab:GetFrameLevel()-1)
-    F.CreateBD(bg)
-
-    select(6, tab:GetRegions()):SetTexCoord(.08, .92, .08, .92)
-end
-
 tinsert(mods["Aurora"], function()
---F.AddPlugin(function()
     local F, C = unpack(Aurora)
     --print("MiscSkins")
     --Travel Pass
