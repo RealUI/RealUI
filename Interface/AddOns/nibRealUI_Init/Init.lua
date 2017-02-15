a9f2177c1b13bd3c49a1e73c776c7879038be8c9
Gethe <gethe88@gmail.com>
2017-02-15 09:29:46 -0600

    Adjust fix for oversized Infobar
    This will now take into account those that do not have retina
    enabled, but have changed the UI scale.

--- Interface/AddOns/nibRealUI_Init/Init.lua
+++ Interface/AddOns/nibRealUI_Init/Init.lua
@@ -100,6 +100,7 @@ local function debug(...)
 end
 
 local _, uiHieght = _G.GetPhysicalScreenSize()
+local pixelScale = 768 / uiHieght
 local uiMod = uiHieght / 768
 function RealUI.ModValue(value, getFloat)
     return RealUI.Round(value * uiMod, getFloat and 2 or 0)
@@ -107,8 +108,11 @@ end
 
 function RealUI.ResetScale(frame)
     -- Frames that are sized via ModValue become HUGE with retina scale.
+    local customScale = RealUI:GetUIScale()
     if RealUI.db.global.tags.retinaDisplay.set then
-        return frame:SetScale(RealUI:GetUIScale())
+        return frame:SetScale(customScale)
+    elseif customScale ~= pixelScale then
+        return frame:SetScale(pixelScale)
     end
 end
 
