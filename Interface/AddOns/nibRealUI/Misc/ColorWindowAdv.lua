cb16533c0395a5cf3f369c3c5ec27db0d1e9b03b
Gethe <gethe88@gmail.com>
2017-02-24 10:57:39 -0600

    Update ColorWindow for changes in Aurora

--- Interface/AddOns/nibRealUI/Misc/ColorWindowAdv.lua
+++ Interface/AddOns/nibRealUI/Misc/ColorWindowAdv.lua
@@ -19,32 +19,22 @@ CPFMover:SetScript("OnMouseUp", function() CPF:StopMovingOrSizing() end)
 CPF:SetUserPlaced(true)
 CPF:EnableKeyboard(false)
 
-local CPFSwatchBG = _G.CreateFrame("Frame", nil, CPF)
-CPFSwatchBG:SetSize(48, 48)
-CPFSwatchBG:SetPoint("TOPLEFT", _G.ColorPickerWheel, "TOPRIGHT", 87, 0)
-for i = 1, 4 do
-    local tex = CPFSwatchBG:CreateTexture(nil, "BACKGROUND", nil, 7)
-    tex:SetTexture([[Tileset\Generic\Checkers]])
-    tex:SetTexCoord(0.03125, 0.21875, 0.28125, 0.46875)
-    tex:SetSize(24, 24)
-    if i == 1 then
-        tex:SetPoint("TOPLEFT", 0, 0)
-    elseif i == 2 then
-        tex:SetPoint("TOPRIGHT", 0, 0)
-    elseif i == 3 then
-        tex:SetPoint("BOTTOMLEFT", 0, 0)
-    elseif i == 4 then
-        tex:SetPoint("BOTTOMRIGHT", 0, 0)
-    end
-end
-local CPFPrevSwatch = CPFSwatchBG:CreateTexture(nil, "BORDER", nil, 7)
-CPFPrevSwatch:SetSize(32, 32)
-CPFPrevSwatch:SetPoint("TOPRIGHT", CPFSwatchBG, "TOPRIGHT", 0, 0)
+CPF.swatchBG = CPF:CreateTexture(nil, "ARTWORK", nil, -8)
+CPF.swatchBG:SetTexture([[Tileset\Generic\Checkers]])
+CPF.swatchBG:SetTexCoord(0.03125, 0.21875, 0.28125, 0.46875)
+CPF.swatchBG:SetSize(48, 48)
+CPF.swatchBG:SetPoint("TOPLEFT", _G.ColorPickerWheel, "TOPRIGHT", 87, 0)
+CPF.swatchBG:SetHorizTile(true)
+CPF.swatchBG:SetVertTile(true)
+
+CPF.prevSwatch = CPF:CreateTexture(nil, "ARTWORK", nil, 7)
+CPF.prevSwatch:SetSize(32, 32)
+CPF.prevSwatch:SetPoint("TOPRIGHT", CPF.swatchBG, "TOPRIGHT", 0, 0)
 
 -- Copy
 local red, green, blue, opacity
 local CPFCopyButton = RealUI:CreateTextButton("Copy", CPF, 60, 22)
-CPFCopyButton:SetPoint("TOP", CPFSwatchBG, "BOTTOM", 0, -5)
+CPFCopyButton:SetPoint("TOP", CPF.swatchBG, "BOTTOM", 0, -5)
 CPFCopyButton:SetScript("OnClick", function()
     red, green, blue = CPF:GetColorRGB()
     opacity = OSF:GetValue()
@@ -75,7 +65,7 @@ CPFSaturation:SetOrientation("VERTICAL")
 CPFSaturation:SetThumbTexture([[Interface\Buttons\UI-SliderBar-Button-Vertical]])
 CPFSaturation:SetHeight(OSF:GetHeight())
 CPFSaturation:SetWidth(OSF:GetWidth())
-CPFSaturation:SetPoint("TOPRIGHT", CPFSwatchBG, "TOPLEFT", -12, 0)
+CPFSaturation:SetPoint("TOPRIGHT", CPF.swatchBG, "TOPLEFT", -12, 0)
 _G["CWA_SatSliderLow"]:ClearAllPoints()
 _G["CWA_SatSliderHigh"]:ClearAllPoints()
 _G["CWA_SatSliderText"]:ClearAllPoints()
@@ -180,43 +170,23 @@ CPF:HookScript("OnShow", function(self)
         local F = _G.Aurora[1]
         F.Reskin(CPFCopyButton)
         F.Reskin(CPFPasteButton)
-        --F.ReskinSlider(CPFSaturation)
+        F.ReskinSlider(CPFSaturation, true)
     end
     if not CPF.moved then
-        _G.ColorPickerFrameHeader:ClearAllPoints()
-        _G.ColorPickerFrameHeader:SetPoint("TOP", CPF, "TOP", 0, 11)
-
-        _G.ColorPickerWheel:ClearAllPoints()
-        _G.ColorPickerWheel:SetPoint("TOPLEFT", CPF, "TOPLEFT", 5, -18)
-
-        local ColorValue = _G.select(6, CPF:GetRegions())
-        ColorValue:ClearAllPoints()
-        ColorValue:SetPoint("LEFT", _G.ColorPickerWheel, "RIGHT", 13, 0)
-
         _G.ColorSwatch:ClearAllPoints()
-        _G.ColorSwatch:SetPoint("BOTTOMLEFT", CPFSwatchBG, "BOTTOMLEFT", 0, 0)
-        _G.ColorSwatch:SetParent(CPFSwatchBG)
+        _G.ColorSwatch:SetPoint("BOTTOMLEFT", CPF.swatchBG, "BOTTOMLEFT", 0, 0)
 
-        OSF:ClearAllPoints()
-        OSF:SetPoint("TOPRIGHT", CPF, "TOPRIGHT", -25, -18)
-
-        _G.ColorPickerOkayButton:ClearAllPoints()
-        _G.ColorPickerOkayButton:SetPoint("BOTTOMLEFT", CPF, "BOTTOMLEFT", 5, 5)
-        _G.ColorPickerOkayButton:SetWidth(100)
-        _G.ColorPickerCancelButton:ClearAllPoints()
-        _G.ColorPickerCancelButton:SetPoint("BOTTOMRIGHT", CPF, "BOTTOMRIGHT", -5, 5)
-        _G.ColorPickerCancelButton:SetWidth(100)
         CPF.moved = true
     end
     local r, g, b = self:GetColorRGB()
     if self.hasOpacity then
-        CPFPrevSwatch:SetTexture(r, g, b, 1 - OSF:GetValue())
+        CPF.prevSwatch:SetTexture(r, g, b, 1 - OSF:GetValue())
         CPFEditBoxes.Alpha:Show()
-        self:SetSize(350, 200)
+        self:SetSize(350, 210)
     else
-        CPFPrevSwatch:SetTexture(r, g, b, 1)
+        CPF.prevSwatch:SetTexture(r, g, b, 1)
         CPFEditBoxes.Alpha:Hide()
-        self:SetSize(280, 200)
+        self:SetSize(280, 210)
     end
 
     UpdateRGBA()
