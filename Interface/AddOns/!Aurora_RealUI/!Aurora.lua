273549ce728eb38591598f5f5f7801e1dadbe563
Gethe <gethe88@gmail.com>
2017-07-08 13:03:06 -0500

    Remove class colors
    We'll let Aurora handle this

--- Interface/AddOns/!Aurora_RealUI/!Aurora.lua
+++ Interface/AddOns/!Aurora_RealUI/!Aurora.lua
@@ -32,21 +32,6 @@ style.defaults = {
     tooltips = false,
 }
 
-style.classcolors = {
-    ["DEATHKNIGHT"] = { r = 0.77, g = 0.12, b = 0.23 },
-    ["DEMONHUNTER"] = { r = 0.64, g = 0.19, b = 0.79 },
-    ["DRUID"]       = { r = 1.00, g = 0.49, b = 0.04 },
-    ["HUNTER"]      = { r = 0.67, g = 0.83, b = 0.45 },
-    ["MAGE"]        = { r = 0.41, g = 0.80, b = 0.94 },
-    ["MONK"]        = { r = 0.00, g = 1.00, b = 0.59 },
-    ["PALADIN"]     = { r = 0.96, g = 0.55, b = 0.73 },
-    ["PRIEST"]      = { r = 0.80, g = 0.80, b = 0.80 },
-    ["ROGUE"]       = { r = 1.00, g = 0.96, b = 0.41 },
-    ["SHAMAN"]      = { r = 0.00, g = 0.44, b = 0.87 },
-    ["WARLOCK"]     = { r = 0.58, g = 0.51, b = 0.79 },
-    ["WARRIOR"]     = { r = 0.78, g = 0.61, b = 0.43 },
-}
-
 local skinnedAtlas = {
     ["token-info-background"] = [[Interface\AddOns\!Aurora_RealUI\Media\AuctionFrame\Token]],
     ["splash-botleft"] = [[Interface\AddOns\!Aurora_RealUI\Media\Splash\Splash]],
