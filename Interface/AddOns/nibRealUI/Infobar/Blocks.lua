bb07acd779c050a391f55059c178f0b60f41d634
Tim West <gethe88@gmail.com>
2018-07-05 02:15:58 -0500

    Undo this change

--- Interface/AddOns/nibRealUI/Infobar/Blocks.lua
+++ Interface/AddOns/nibRealUI/Infobar/Blocks.lua
@@ -426,7 +426,7 @@ end
 
 local function SetupTooltip(tooltip, block)
     tooltip:SetHeaderFont("Fancy16Font")
-    tooltip:SetFontObject("SystemFont_Shadow_Med1")
+    tooltip:SetFont("SystemFont_Shadow_Med1")
     tooltip:SmartAnchorTo(block)
     tooltip:SetAutoHideDelay(0.10, block)
     block.tooltip = tooltip
