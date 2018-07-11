230ebb3d3b142eeda1cc45b0922bb0b5db843808
Tim West <gethe88@gmail.com>
2018-07-11 01:58:03 -0500

    Rearrange project folders
    This is primarily being done to better conform with what the packager
    expects.

--- package.bat
+++ package.bat
@@ -1,25 +1,22 @@
-:: Run luacheck
-call luacheck Interface || pause && EXIT
-
 :: Increment version
 powershell .\utils\update.ps1
 
 :: Run packager
-:: Usage: release.sh [-acdelosuz] [-t topdir] [-r releasedir] [-p slug] [-w wowi-id] [-g game-version]
-::   -a               Skip third party addons.
+:: Usage: release.sh [-cdelLosuz] [-t topdir] [-r releasedir] [-p curse-id] [-w wowi-id] [-g game-version]
 ::   -c               Skip copying files into the package directory.
 ::   -d               Skip uploading.
 ::   -e               Skip checkout of external repositories.
 ::   -l               Skip @localization@ keyword replacement.
+::   -L               Only do @localization@ keyword replacement (skip upload to CurseForge).
 ::   -o               Keep existing package directory, overwriting its contents.
 ::   -s               Create a stripped-down "nolib" package.
 ::   -u               Use Unix line-endings.
-::   -z               Skip zipfile creation.
+::   -z               Skip zip file creation.
 ::   -t topdir        Set top-level directory of checkout.
-::   -r releasedir    Set directory containing the package directory. Defaults to $topdir/.release.
+::   -r releasedir    Set directory containing the package directory. Defaults to "$topdir/.release".
 ::   -p curse-id      Set the project id used on CurseForge for localization and uploading.
 ::   -w wowi-id       Set the addon id used on WoWInterface for uploading.
-::   -g game-version  Set the game version to use for CurseForge and WoWInterface uploading."
+::   -g game-version  Set the game version to use for CurseForge uploading.
 bash -c "./utils/release.sh -do -p 88269 -w 16068"
 
 pause
