1fe937669669d3146fc68ab247d873784a393fb0
Gethe <gethe88@gmail.com>
2019-01-28 15:05:41 -0600

    Use the keys from the latest version

--- RealUI_Skins/AddOns/Ace3.lua
+++ RealUI_Skins/AddOns/Ace3.lua
@@ -257,10 +257,9 @@ function private.AddOns.Ace3()
                 Base.SetBackdrop(bg, frameColor)
 
                 widget.colorSwatch:Hide()
-                local texture, checkers = select(2, widget.frame:GetRegions())
-                texture:Hide()
-                checkers:SetAllPoints(bg)
-                checkers:SetDrawLayer("BACKGROUND")
+                widget.background:Hide()
+                widget.checkers:SetAllPoints(bg)
+                widget.checkers:SetDrawLayer("BACKGROUND")
             end,
             ["Dropdown-Item"] = function(widget)
                 local highlight = widget.highlight
