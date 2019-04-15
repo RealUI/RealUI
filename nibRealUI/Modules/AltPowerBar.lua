26c683bf5eb871a3201985a473154bbcaeb1b5a3
Gethe <gethe88@gmail.com>
2019-04-15 00:01:37 -0500

    Update AltPowerBar skin

--- nibRealUI/Modules/AltPowerBar.lua
+++ nibRealUI/Modules/AltPowerBar.lua
@@ -6,9 +6,9 @@ local db
 
 local MODNAME = "AltPowerBar"
 local AltPowerBar = RealUI:NewModule(MODNAME, "AceEvent-3.0")
+local Aurora = _G.Aurora
 
 local LoggedIn = false
-local APBFrames = {}
 local UpdateInterval = 0
 
 --[[ Testing spots:
@@ -29,31 +29,22 @@ local UpdateInterval = 0
 -- Events
 function AltPowerBar:PowerUpdate()
     if _G.UnitAlternatePowerInfo("player") then
-        APBFrames.bg:Show()
-        APBFrames.bar:Show()
+        self.bar:Show()
     else
-        APBFrames.bg:Hide()
-        APBFrames.bar:Hide()
+        self.bar:Hide()
     end
-end
-
--- Colors
-function AltPowerBar:UpdateColors()
-    -- Bar
-    local color = RealUI.media.colors.green
-    APBFrames.bar:SetStatusBarColor(color[1], color[2], color[3], 0.85)
 end
 
 -- Position
 function AltPowerBar:UpdatePosition()
-    -- BG + Border
-    APBFrames.bg:SetPoint(db.position.anchorfrom, _G.UIParent, db.position.anchorto, db.position.x, db.position.y)
+    local bar = self.bar
+    bar:SetPoint(db.position.anchorfrom, _G.UIParent, db.position.anchorto, db.position.x, db.position.y)
 
-    APBFrames.bg:SetFrameStrata("MEDIUM")
-    APBFrames.bg:SetFrameLevel(1)
+    bar:SetFrameStrata("MEDIUM")
+    bar:SetFrameLevel(1)
 
-    APBFrames.bg:SetHeight(db.size.height)
-    APBFrames.bg:SetWidth(db.size.width)
+    bar:SetHeight(db.size.height)
+    bar:SetWidth(db.size.width)
 end
 
 -- Refresh
@@ -62,61 +53,47 @@ function AltPowerBar:RefreshMod()
 
     db = self.db.profile
 
-    AltPowerBar:UpdatePosition()
-    AltPowerBar:UpdateColors()
+    self:UpdatePosition()
+    self:PowerUpdate()
 end
 
 function AltPowerBar:PLAYER_LOGIN()
     LoggedIn = true
-    AltPowerBar:RefreshMod()
-    AltPowerBar:PowerUpdate()
+    self:RefreshMod()
 end
 
 -- Create Frames
 function AltPowerBar:CreateFrames()
-    APBFrames = {
-        bg = nil,
-        bar = nil,
-        text = nil,
-    }
+    local bar = _G.CreateFrame("StatusBar", "RealUI_AltPowerBar", _G.UIParent)
+    Aurora.Skin.FrameTypeStatusBar(bar)
+    bar:SetStatusBarColor(Aurora.Color.green:GetRGB())
+    bar:SetPoint(db.position.anchorfrom, _G.UIParent, db.position.anchorto, db.position.x, db.position.y)
+    self.bar = bar
 
-    -- BG + Border
-    APBFrames.bg = _G.CreateFrame("Frame", "RealUI_AltPowerBarBG", _G.UIParent)
-    APBFrames.bg:SetPoint(db.position.anchorfrom, _G.UIParent, db.position.anchorto, db.position.x, db.position.y)
-    _G.Aurora.Base.SetBackdrop(APBFrames.bg)
-
-    -- Bar + Text
-    APBFrames.bar = _G.CreateFrame("StatusBar", "RealUI_AltPowerBar", APBFrames.bg)
-    APBFrames.bar:SetStatusBarTexture(RealUI.media.textures.plain)
-    APBFrames.bar:SetMinMaxValues(0, 100)
-    APBFrames.bar:SetPoint("TOPLEFT", APBFrames.bg, "TOPLEFT", 1, -1)
-    APBFrames.bar:SetPoint("BOTTOMRIGHT", APBFrames.bg, "BOTTOMRIGHT", -1, 1)
-
-    APBFrames.text = APBFrames.bar:CreateFontString(nil, "OVERLAY")
-    APBFrames.text:SetPoint("CENTER", APBFrames.bar)
-    APBFrames.text:SetFontObject("SystemFont_Shadow_Med1")
-    APBFrames.text:SetTextColor(1, 1, 1, 1)
+    local text = bar:CreateFontString(nil, "OVERLAY")
+    text:SetPoint("CENTER", bar)
+    text:SetFontObject("SystemFont_Shadow_Med1")
+    text:SetTextColor(1, 1, 1, 1)
+    bar.text = text
 
     -- Update Power
     UpdateInterval = 0
-    APBFrames.bar:SetScript("OnUpdate", function(bar, elapsed)
+    bar:SetScript("OnUpdate", function(this, elapsed)
         UpdateInterval = UpdateInterval + elapsed
 
         if UpdateInterval > 0.1 then
-            bar:SetMinMaxValues(0, _G.UnitPowerMax("player", _G.ALTERNATE_POWER_INDEX))
             local CurPower = _G.UnitPower("player", _G.ALTERNATE_POWER_INDEX)
             local MaxPower = _G.UnitPowerMax("player", _G.ALTERNATE_POWER_INDEX)
-            bar:SetValue(CurPower)
+            this:SetMinMaxValues(0, MaxPower)
+            this:SetValue(CurPower)
             if MaxPower > 0 then
-                APBFrames.text:SetText(CurPower.."/"..MaxPower)
+                text:SetText(CurPower.."/"..MaxPower)
             else
-                APBFrames.text:SetText("0")
+                text:SetText("0")
             end
             UpdateInterval = 0
         end
     end)
-
-    APBFrames.bg:Hide()
 end
 
 -- Initialize
@@ -137,7 +114,7 @@ function AltPowerBar:OnInitialize()
 
     self:SetEnabledState(RealUI:GetModuleEnabled(MODNAME))
 
-    AltPowerBar:CreateFrames()
+    self:CreateFrames()
 end
 
 function AltPowerBar:OnEnable()
@@ -150,8 +127,7 @@ function AltPowerBar:OnEnable()
     _G.PlayerPowerBarAlt:SetAlpha(0)
 
     if LoggedIn then
-        AltPowerBar:RefreshMod()
-        AltPowerBar:PowerUpdate()
+        self:RefreshMod()
     end
 end
 
@@ -161,6 +137,6 @@ function AltPowerBar:OnDisable()
     self:UnregisterEvent("UNIT_POWER_BAR_SHOW")
     self:UnregisterEvent("UNIT_POWER_BAR_HIDE")
 
-    APBFrames.bg:Hide()
+    self.bar:Hide()
     _G.PlayerPowerBarAlt:SetAlpha(1)
 end
