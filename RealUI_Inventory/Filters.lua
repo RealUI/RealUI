65f56d4831edd74bcb5bd1ef45f79c231eb51d35
Gethe <gethe88@gmail.com>
2019-11-17 09:57:16 -0600

    Refactor trade goods filters

--- RealUI_Inventory/Filters.lua
+++ RealUI_Inventory/Filters.lua
@@ -3,6 +3,9 @@ local _, private = ...
 -- Lua Globals --
 -- luacheck: globals tinsert ipairs
 
+-- RealUI --
+local Inventory = private.Inventory
+
 private.filters = {}
 private.filterList = {}
 local function CreateFilter(tag, info)
@@ -26,28 +29,24 @@ CreateFilter("questitems", {
     end,
 })
 
-local tradegoods, prefix = {}, _G.BAG_FILTER_TRADE_GOODS .. ": %s"
-local classIndex = _G.LE_ITEM_CLASS_TRADEGOODS
-local subClassIndex = 0
-local subClassName = _G.GetItemSubClassInfo(classIndex, subClassIndex)
-
-while subClassName and subClassName ~= "" do
-    tradegoods[subClassIndex] = subClassName
-
-    subClassIndex = subClassIndex + 1
-    subClassName = _G.GetItemSubClassInfo(classIndex, subClassIndex)
+local prefix = _G.BAG_FILTER_TRADE_GOODS .. ": %s"
+local tradegoods
+if Inventory.isPatch then
+    tradegoods = _G.C_AuctionHouse.GetAuctionItemSubClasses(_G.LE_ITEM_CLASS_TRADEGOODS)
+else
+    tradegoods = {_G.GetAuctionItemSubClasses(_G.LE_ITEM_CLASS_TRADEGOODS)}
 end
 
-for i = 0, #tradegoods do
-    if not tradegoods[i]:find("OBSOLETE") then
-        CreateFilter("tradegoods_"..tradegoods[i], {
-            name = prefix:format(tradegoods[i]),
-            filter = function(slot)
-                local _, _, _, _, _, typeID, subTypeID = _G.GetItemInfoInstant(slot.item:GetItemID())
-                return typeID == _G.LE_ITEM_CLASS_TRADEGOODS and subTypeID == i
-            end,
-        })
-    end
+for i = 1, #tradegoods do
+    local subClassID = tradegoods[i]
+    local name = _G.GetItemSubClassInfo(_G.LE_ITEM_CLASS_TRADEGOODS, subClassID)
+    CreateFilter("tradegoods_"..name, {
+        name = prefix:format(name),
+        filter = function(slot)
+            local _, _, _, _, _, typeID, subTypeID = _G.GetItemInfoInstant(slot.item:GetItemID())
+            return typeID == _G.LE_ITEM_CLASS_TRADEGOODS and subTypeID == subClassID
+        end,
+    })
 end
 
 CreateFilter("equipment", {
