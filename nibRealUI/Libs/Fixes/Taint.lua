7264d9cf9b380f0c2bbec6bf4fa080ba2ee3f273
Gethe <gethe88@gmail.com>
2020-01-05 08:36:33 -0600

    Update taint fixes

--- nibRealUI/Libs/Fixes/Taint.lua
+++ nibRealUI/Libs/Fixes/Taint.lua
@@ -22,28 +22,58 @@ end
 
 -- UIDropDownMenu_SetSelectedValue/_Refresh can taint execution
 -- https://www.townlong-yak.com/bugs/YhgQma-SetValueRefreshTaint
-if (COMMUNITY_UIDD_REFRESH_PATCH_VERSION or 0) < 1 then
-    COMMUNITY_UIDD_REFRESH_PATCH_VERSION = 1
-    local function CleanDropdowns()
-        if COMMUNITY_UIDD_REFRESH_PATCH_VERSION ~= 1 then
-            return
-        end
-        local f, f2 = FriendsFrame, FriendsTabHeader
-        local s = f:IsShown()
-        f:Hide()
-        f:Show()
-        if not f2:IsShown() then
-            f2:Show()
-            f2:Hide()
-        end
-        if not s then
+if (COMMUNITY_UIDD_REFRESH_PATCH_VERSION or 0) < 2 then
+    COMMUNITY_UIDD_REFRESH_PATCH_VERSION = 2
+    if select(4, GetBuildInfo()) > 8e4 then
+        local function CleanDropdowns()
+            if COMMUNITY_UIDD_REFRESH_PATCH_VERSION ~= 2 then
+                return
+            end
+            local f, f2 = FriendsFrame, FriendsTabHeader
+            local s = f:IsShown()
             f:Hide()
+            f:Show()
+            if not f2:IsShown() then
+                f2:Show()
+                f2:Hide()
+            end
+            if not s then
+                f:Hide()
+            end
+        end
+        hooksecurefunc("Communities_LoadUI", CleanDropdowns)
+        hooksecurefunc("SetCVar", function(n)
+            if n == "lastSelectedClubId" then
+                CleanDropdowns()
+            end
+        end)
+    end
+end
+
+-- UIDropDownMenu_Refresh accesses uninitialized buttons
+-- https://www.townlong-yak.com/bugs/Mx7CWN-RefreshOverread
+if (UIDD_REFRESH_OVERREAD_PATCH_VERSION or 0) < 1 then
+    UIDD_REFRESH_OVERREAD_PATCH_VERSION = 1
+    local function drop(t, k)
+        local c = 42
+        t[k] = nil
+        while not issecurevariable(t, k) do
+            if t[c] == nil then
+                t[c] = nil
+            end
+            c = c + 1
         end
     end
-    hooksecurefunc("Communities_LoadUI", CleanDropdowns)
-    hooksecurefunc("SetCVar", function(n)
-        if n == "lastSelectedClubId" then
-            CleanDropdowns()
+    hooksecurefunc("UIDropDownMenu_InitializeHelper", function()
+        if UIDD_REFRESH_OVERREAD_PATCH_VERSION ~= 1 then
+            return
+        end
+        for i=1,UIDROPDOWNMENU_MAXLEVELS do
+            for j=1,UIDROPDOWNMENU_MAXBUTTONS do
+                local b, _ = _G["DropDownList" .. i .. "Button" .. j]
+                _ = issecurevariable(b, "checked")      or drop(b, "checked")
+                _ = issecurevariable(b, "notCheckable") or drop(b, "notCheckable")
+            end
         end
     end)
 end
