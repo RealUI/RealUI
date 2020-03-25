581d748d36b2da5398e3c9ce2e9e79730b99bd5b
Gethe <gethe88@gmail.com>
2020-03-25 12:40:09 -0500

    Add support for LibSink

--- RealUI_CombatText/RealUI_CombatText.lua
+++ RealUI_CombatText/RealUI_CombatText.lua
@@ -211,26 +211,24 @@ function CombatText:OnInitialize()
     private.CreateScrollAreas()
 
 
-    --[[
     local LibSink = _G.LibStub("LibSink-2.0")
     if LibSink then
+        local textFormat = "|c%s%s|r"
         local function sink(addon, text, r, g, b, font, size, outline, sticky, location, icon)
-            local storage = LibSink.storageForAddon[addon]
-            if storage then
-                location = storage.sink20ScrollArea or location or "Notification"
-                sticky = storage.sink20Sticky or sticky
-            end
-            self:ShowMessage(text, location, sticky, r, g, b, font, size, outline, icon)
-        end
-        local function getScrollAreasChoices()
-            local tmp = {}
-            for k, v in next, self:GetScrollAreasChoices() do
-                tmp[#tmp+1] = v
-            end
-            return tmp
+            local eventInfo = {
+                string = textFormat:format(RealUI.GetColorString(r, g, b), text),
+                icon = icon,
+                scrollType = location or "notification",
+                isSticky = sticky,
+            }
+            private.AddEvent(eventInfo)
         end
 
-        LibSink:RegisterSink("RealUI_CombatText", "RealUI_CombatText", nil, sink, getScrollAreasChoices, true)
+        local scrollAreas = {"incoming", "outgoing", "notification"}
+        local function getScrollAreasChoices()
+            return scrollAreas
+        end
+
+        LibSink:RegisterSink("CombatText", "RealUI CombatText", nil, sink, getScrollAreasChoices, true)
     end
-    ]]
 end
