2f0ed3571d5d367797e0020901fcfc1cf26f50ec
Arnvid Karstad <arnvid@isd.no>
2024-11-02 00:31:18 +0100

    chg: removed debug threat spam :)

--- nibRealUI/HuD/UnitFrames/Tags.lua
+++ nibRealUI/HuD/UnitFrames/Tags.lua
@@ -225,9 +225,9 @@ tags.Methods["realui:threat"] = function(unit)
 
         if percentage and percentage ~= 0 then
             UnitFrames:debug("threat", color, isTanking, percentage)
-            if _G.RealUI.isDev then
-                _G.print(("Current threat: - %s%1.0f%%|r"):format(color, percentage))
-            end
+            -- if _G.RealUI.isDev then
+            --     _G.print(("Current threat: - %s%1.0f%%|r"):format(color, percentage))
+            -- end
             return ("%s%1.0f%%|r"):format(color, percentage)
         end
     end
