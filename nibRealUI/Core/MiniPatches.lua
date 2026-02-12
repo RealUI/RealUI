8517c960e1d2704ce577827a8e6c234922378597
Arnvid Karstad <arnvid@isd.no>
2026-02-12 01:27:02 +0100

    chg: patch for reverse missing.

--- nibRealUI/Core/MiniPatches.lua
+++ nibRealUI/Core/MiniPatches.lua
@@ -8,22 +8,19 @@ local RealUI = private.RealUI
 --local debug = RealUI.GetDebug("MiniPatch")
 
 RealUI.minipatches = {
-    --[[
     [1] = function()
-        debug("patch 1")
-        for profileName, profile in next, _G.nibRealUIDB.profiles do
-            local settings = profile.settings
-            -- This setting was doing the opposite of what it should, so hopefully
-            -- reversing it will make the change transparent to most users.
-            if settings and settings.reverseUnitFrameBars == nil then
-                settings.reverseUnitFrameBars = not settings.reverseUnitFrameBars
+        if not (_G.nibRealUIDB and _G.nibRealUIDB.profiles) then
+            return
+        end
+        for _, profile in next, _G.nibRealUIDB.profiles do
+            local units = profile and profile.units
+            if units then
+                for _, unitInfo in next, units do
+                    if type(unitInfo) == "table" and unitInfo.reverseMissing ~= nil then
+                        unitInfo.reverseMissing = nil
+                    end
+                end
             end
         end
     end,
-    [0] = function()
-        debug("patch 0")
-        for profileName, profile in next, _G.nibRealUIDB.profiles do
-        end
-    end,
-    ]]
 }
