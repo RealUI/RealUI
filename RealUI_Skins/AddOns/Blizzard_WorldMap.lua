f1a22a1d20adafdac82af001c70bc988fa34d6fb
Arnvid Karstad <arnvid@isd.no>
2026-02-19 00:07:02 +0100

    fix: World map taint error causing ADDON_ACTION_BLOCKED when opening map (RealUI_Skins)

--- RealUI_Skins/AddOns/Blizzard_WorldMap.lua
+++ RealUI_Skins/AddOns/Blizzard_WorldMap.lua
@@ -12,12 +12,12 @@ local HBD --= _G.LibStub("HereBeDragons-2.0")
 
 do --[[ AddOns\Blizzard_WorldMap.lua ]]
     local colorStr = Color.highlight.colorStr
-    local round, ticker = _G.RealUI.Scale.Round
+    local round = _G.RealUI.Scale.Round
+    local ticker
 
     local coordinateFormat = ("|c%s%s"):format(colorStr, "%s: |cffffffff%s, %s|r")
     local coordinateUnavailable = ("|c%s%s: |cffffffff%s|r"):format(colorStr, _G.PLAYER, _G.UNAVAILABLE)
-    local function updateCoords(self)
-        local coords = self._ruiCoords
+    local function updateCoords(coords, mapFrame)
         if not HBD then
             HBD = _G.LibStub("HereBeDragons-2.0")
         end
@@ -34,8 +34,8 @@ do --[[ AddOns\Blizzard_WorldMap.lua ]]
         end
 
         -- Mouse
-        if self.ScrollContainer:IsMouseOver() then
-            local cursorX, cursorY = self:GetNormalizedCursorPosition()
+        if mapFrame.ScrollContainer:IsMouseOver() then
+            local cursorX, cursorY = mapFrame:GetNormalizedCursorPosition()
 
             cursorX = round(100 * cursorX, 1)
             cursorY = round(100 * cursorY, 1)
@@ -45,14 +45,35 @@ do --[[ AddOns\Blizzard_WorldMap.lua ]]
         end
     end
 
-    function Hook.WorldMapMixin_OnShow(self)
-        ticker = _G.C_Timer.NewTicker(0.1, function(this, elapsed)
-            updateCoords(self)
+    -- Create a monitoring frame that doesn't taint WorldMapFrame
+    local monitorFrame = _G.CreateFrame("Frame")
+    monitorFrame:Hide()
+
+    function Hook.StartCoordinateTracking(coords, mapFrame)
+        if ticker then
+            ticker:Cancel()
+        end
+        ticker = _G.C_Timer.NewTicker(0.1, function()
+            updateCoords(coords, mapFrame)
         end)
+        monitorFrame:Show()
     end
-    function Hook.WorldMapMixin_OnHide(self)
-        ticker:Cancel()
+
+    function Hook.StopCoordinateTracking()
+        if ticker then
+            ticker:Cancel()
+            ticker = nil
+        end
+        monitorFrame:Hide()
     end
+
+    -- Monitor WorldMapFrame visibility without hooking its scripts
+    monitorFrame:SetScript("OnUpdate", function(self)
+        local mapFrame = _G.WorldMapFrame
+        if mapFrame and not mapFrame:IsShown() then
+            Hook.StopCoordinateTracking()
+        end
+    end)
 end
 
 --[[ do AddOns\Blizzard_WorldMap.xml
@@ -60,9 +81,8 @@ end ]]
 
 _G.hooksecurefunc(private.AddOns, "Blizzard_WorldMap", function()
     local WorldMapFrame = _G.WorldMapFrame
-    WorldMapFrame:HookScript("OnShow", Hook.WorldMapMixin_OnShow)
-    WorldMapFrame:HookScript("OnHide", Hook.WorldMapMixin_OnHide)
 
+    -- Create coordinate display elements
     local player = WorldMapFrame.BorderFrame:CreateFontString(nil, "OVERLAY")
     player:SetPoint("LEFT", WorldMapFrame.BorderFrame.TitleContainer, -20, 0)
     player:SetFontObject(_G.SystemFont_Shadow_Med1)
@@ -75,8 +95,25 @@ _G.hooksecurefunc(private.AddOns, "Blizzard_WorldMap", function()
     mouse:SetJustifyH("LEFT")
     mouse:SetText("")
 
-    WorldMapFrame._ruiCoords = {
+    local coords = {
         player = player,
         mouse = mouse
     }
+
+    -- Use an event frame to track map visibility without tainting
+    local eventFrame = _G.CreateFrame("Frame")
+    eventFrame:RegisterEvent("WORLD_MAP_OPEN")
+    eventFrame:RegisterEvent("WORLD_MAP_CLOSE")
+    eventFrame:SetScript("OnEvent", function(self, event)
+        if event == "WORLD_MAP_OPEN" then
+            Hook.StartCoordinateTracking(coords, WorldMapFrame)
+        elseif event == "WORLD_MAP_CLOSE" then
+            Hook.StopCoordinateTracking()
+        end
+    end)
+
+    -- Start tracking if map is already open
+    if WorldMapFrame:IsShown() then
+        Hook.StartCoordinateTracking(coords, WorldMapFrame)
+    end
 end)
